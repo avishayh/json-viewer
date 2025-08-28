d42a3686bcba2b6daa646ecea75fab49e2c11173
avishayh <avishayh@jfrog.com>
2025-08-28 16:48:47 +0300

    fix vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,16 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import { fileURLToPath, URL } from 'node:url'
+import { viteSingleFile } from 'vite-plugin-singlefile'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue(), viteSingleFile()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   },
-  publicDir: 'src/examples',
   server: {
     fs: {
       allow: ['..']
